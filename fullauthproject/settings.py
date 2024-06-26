fd0dbbae3e004db53701424290b575baaf88bc28
grace <graceogwe@gmail.com>
2024-06-26 16:19:34 +0100

    nineth commit

--- fullauthproject/settings.py
+++ fullauthproject/settings.py
@@ -128,4 +128,4 @@ STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATICFILES_DIRS=os.path.join(BASE_DIR,'static')
+STATICFILES_DIRS=BASE_DIR/'static'
