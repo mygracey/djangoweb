122568508c73cfb83443827b58a4f1be48cacab6
grace <graceogwe@gmail.com>
2024-06-26 16:05:43 +0100

    sixth commit

--- fullauthproject/settings.py
+++ fullauthproject/settings.py
@@ -121,11 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT=BASE_DIR,'/assets'
+STATIC_ROOT=os.path.join(BASE_DIR,'/assets')
 STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATICFILES_DIRS=['static/']
+STATICFILES_DIRS=os.path.join(BASE_DIR,'static')
